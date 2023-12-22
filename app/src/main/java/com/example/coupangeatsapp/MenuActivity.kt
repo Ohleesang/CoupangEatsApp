02bb85b4ab3a13ca15c661931aa7bd115e08941e
Ohleesang <jammmmed@gmail.com>
2023-12-22 10:57:40 +0900

    Feat : add searchView 검색기능 onQueryTextSumit(query:String?) : 검색 바에 입력된 문자열로 googleMap 앱에 암시적 인텐트로 전달

--- app/src/main/java/com/example/coupangeatsapp/MenuActivity.kt
+++ app/src/main/java/com/example/coupangeatsapp/MenuActivity.kt
@@ -1,11 +1,38 @@
 package com.example.coupangeatsapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 
 class MenuActivity : AppCompatActivity() {
+
+    val searchView: SearchView? by lazy { findViewById<SearchView>(R.id.searchView) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+
+
+        //searchView 기능 구현
+        searchView?.setOnQueryTextListener(object : OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //암시적 인텐트
+                val uri = Uri.parse("geo:0,0?q=$query")
+                var intent = Intent(Intent.ACTION_VIEW,uri)
+                intent.setPackage("com.google.android.apps.maps")
+                startActivity(intent)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
     }
 }
+
